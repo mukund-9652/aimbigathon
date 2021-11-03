0bd9713b1e0c525b0d7dede44434c85946c8ba33
mukund-9652 <mukundravisankaran2001@gmail.com>
2021-11-03 16:32:49 +0530

    first commit

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2021, 11, 29, 10)}); // year, month, date, hour
+        });
+});		
+
